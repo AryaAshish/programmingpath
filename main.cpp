741a1e575c44c87436dacb838583c3dd0f37d50b
ashisharyanalyv <Aryan971998@gmail.com>
2021-03-08 17:47:00 +0000

    prefixmax

--- main.cpp
+++ main.cpp
@@ -1,5 +1,30 @@
 #include <iostream>
+#include <stdio.h>
+
+int prefixSum(int array[], int n, int prefixsum[],int prefixmax[])
+{
+   prefixsum[0] = array[0];
+   prefixmax[0] = array[0];
+for(int i=1;i<n;i++){
+prefixsum[i]= array[i]+prefixsum[i-1];
+if(prefixmax[i-1]<array[i]){
+  prefixmax[i]=array[i];
+}else{
+    prefixmax[i]=prefixmax[i-1];
+}
+}
+return prefixmax[n];
+}
 
 int main() {
-  std::cout << "Hello World!\n";
+
+ int arr[] = { 10, 4, 16, 20,12,23,1,5,70,3,4 };
+    int n = sizeof(arr) / sizeof(arr[0]);
+    int prefixdum[n];
+    int prefixMax[n];
+ 
+    prefixSum(arr, n, prefixdum,prefixMax);
+    for (int i = 0; i < n; i++)
+        std::cout << prefixMax[i] << " ";
+  
 }
