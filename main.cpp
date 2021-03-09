35c8d2c72eb06bebc83eddcfe188eee9dcc43916
ashisharyanalyv <Aryan971998@gmail.com>
2021-03-09 04:30:07 +0000

    prefix +suffix sum and max

--- main.cpp
+++ main.cpp
@@ -16,14 +16,29 @@ if(prefixmax[i-1]<array[i]){
 return prefixmax[n];
 }
 
+int suffixSum(int array[], int n, int suffixsum[],int suffixmax[])
+{
+   suffixsum[n] = array[n];
+   suffixmax[n] = array[n];
+for(int i=n-1;i>=0;i--){
+suffixsum[i]= array[i]+suffixsum[i+1];
+if(suffixmax[i+1]<array[i]){
+  suffixmax[i]=array[i];
+}else{
+    suffixmax[i]=suffixmax[i+1];
+}
+}
+return suffixmax[n];
+}
+
 int main() {
 
- int arr[] = { 10, 4, 16, 20,12,23,1,5,70,3,4 };
+ int arr[] = { 10, 4, 16, 20,12,2,1,5,7,3,4 };
     int n = sizeof(arr) / sizeof(arr[0]);
     int prefixdum[n];
     int prefixMax[n];
  
-    prefixSum(arr, n, prefixdum,prefixMax);
+    suffixSum(arr, n, prefixdum,prefixMax);
     for (int i = 0; i < n; i++)
         std::cout << prefixMax[i] << " ";
   
